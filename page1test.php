d62296b7fac4528e9e993682e6e666dd2e131115
aalby4 <antoine.alby@edu.ece.fr>
2018-05-04 16:00:45 +0200

    inscription fonctionnelle

--- page1test.php
+++ page1test.php
@@ -59,7 +59,7 @@ if ($db_found)
 			if ($MDP=="" || $ID=="") // Si le champs mdp n'est pas vide
 				{
 						
-						echo 'aucargf ';
+						echo 'Les champs ne sont pas rempli ';
 			 
 				} 
 				
@@ -84,20 +84,97 @@ echo '</div>';
 <div id="img3" class="body"><img src="ece2.png"/></div>
 
 <!-- Formulaire pour créer un compte -->
-<form method="post" action=""=>
+
+
+<?php
+
+echo '<form method="post" =>
 <div id="form3" class="body">
-<input placeholder="Pseudo" type="text" class="namebox" /><br>
-<input placeholder="Prénom" type="text" class="namebox" name="name1" />
-<input placeholder="Nom" type="text" class="namebox" /></br>
-<input type="date" class="namebox" /></br>
-<input placeholder="Email" type="text" class="namebox" /></br>
-<input placeholder="Mot de passe" type="password" class="namebox" /></br>
-<input placeholder="Confirmer le mot de passe" type="password" class="namebox" /></br></br>
+<input id="pseudo" placeholder="Pseudo" type="text" class="namebox" name="pseudo" /><br>
+<input id="name" placeholder="Prénom" type="text" class="namebox" name="name" />
+<input id="lastName" placeholder="Nom" type="text" class="namebox" name=lastName /></br>
+<input id="birth" type="date" class="namebox" name="birth" /></br>
+<input id="email" placeholder="Email" type="text" class="namebox" name="email" /></br>
+<input id= "password" placeholder="Mot de passe" type="password" class="namebox" name="password" /></br>
+<input id="passwordCheck" placeholder="Confirmer le mot de passe" type="password" class="namebox" name="passwordCheck" /></br></br>
 <div id="mess3" class="form3"> En cliquant sur "Inscription" vous vous engagez à accepter nos termes et conditions. <br></div>
 <input type="submit" class="bouton2" value="Inscription"/>
 
 </div>
-</form>
+</form>';
+
+
+
+
+
+	
+
+//on recupere les donnees saisie par l'utilisateur
+
+	$PSEUDO = isset($_POST["pseudo"])? $_POST["pseudo"]: ""; 
+	$NAME = isset($_POST["name"])? $_POST["lastName"]: ""; 
+	$LASTNAME = isset($_POST["lastName"])? $_POST["lastName"]: ""; 
+	$BIRTH = isset($_POST["birth"])? $_POST["birth"]: ""; 
+	$EMAIL = isset($_POST["email"])? $_POST["email"]: ""; 
+	$PASSWORD = isset($_POST["password"])? $_POST["password"]: ""; 
+	$PASSWORDCHECK = isset($_POST["passwordCheck"])? $_POST["passwordCheck"]: ""; 
+
+if ($db_found)
+{
+	
+if( $PSEUDO=="" || $NAME=="" || $LASTNAME=="" || $BIRTH=="" || $EMAIL=="" || $PASSWORD=="" || $PASSWORDCHECK=="")
+			 {
+				 echo "Tous les champs ne sont pas rempli";
+			 }
+
+else
+{
+
+	$sql_checkPseudo= "SELECT pseudo FROM utilisateur WHERE pseudo='$PSEUDO'";
+	$result=mysqli_query($db_handle, $sql_checkPseudo);
+	
+	// On verifie les champs rentre
+	 $data=mysqli_fetch_assoc($result);
+	
+		 
+		if ((!$data) && ($PASSWORD==$PASSWORDCHECK)) // Si pseudo n'existe pas et les deux mots de passe saisit sont equivalents
+		 {
+			 echo "test";	
+
+			 $sql= "INSERT INTO `utilisateur` (`pseudo`, `nom`, `prenom`, `email`, `mdp`, `date_naissance`, `tel`, `sexe`, `statut_pro`, `id_statut`, `id_formation`, `id_experience`, `id_interet`, `id_competence`)
+		 VALUES ('$PSEUDO','$LASTNAME','$NAME', '$EMAIL', '$PASSWORD','$BIRTH', 00000000, '', 'inconnu', NULL, NULL, NULL, NULL, NULL)" ;
+		 
+		 mysqli_query($db_handle, $sql);
+		 }
+		 
+		 else
+		 {		 
+			 if ($PASSWORD!=$PASSWORDCHECK)
+			 {
+				echo "failMdp"; // Si les mdp ne sont pas identiques
+			 }
+			 
+			echo "Ce pseudo existe deja"; // Si le pseudo existe deja
+		 }
+		
+		
+	
+ }
+}	
+			
+
+			
+	
+	
+
+
+else
+{
+	echo "echec de connexion a la bdd";
+	
+}
+
+?>	
 
 
 <!-- Barre en bas de la page --> 
